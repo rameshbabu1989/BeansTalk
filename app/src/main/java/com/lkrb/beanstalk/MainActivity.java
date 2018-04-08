5938d1fbfa985529921eeda8834b9e0077cc1c7d
Ramesh Babu <ramesh@eternushealth.com>
2018-04-08 13:07:19 +0530

    Project structure creation

---
+++ app/src/main/java/com/lkrb/beanstalk/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lkrb.beanstalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
