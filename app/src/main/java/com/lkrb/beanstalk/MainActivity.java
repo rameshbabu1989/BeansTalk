b75d13a9d2abf2adf43ed62c93598333b264967a
Ramesh Babu <ramesh@eternushealth.com>
2018-04-08 14:24:33 +0530

    Login Page created

--- app/src/main/java/com/lkrb/beanstalk/MainActivity.java
+++ app/src/main/java/com/lkrb/beanstalk/MainActivity.java
@@ -1,14 +1,8 @@
 package com.lkrb.beanstalk;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -21,23 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-        mAuth.createUserWithEmailAndPassword("s.rameshbabu1989@gmail.com", "Password@123")
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Log.i(TAG, "onComplete: "+user.getEmail());
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            Log.i(TAG, "onComplete: fail");
-                        }
-                    }
-                });
+        FirebaseUser user =mAuth.getCurrentUser();
+        if(user!=null){
+            TextView textView= (TextView) findViewById(R.id.hello);
+            textView.setText("Hi "+user.getEmail().toString());
+        }
     }
 }
