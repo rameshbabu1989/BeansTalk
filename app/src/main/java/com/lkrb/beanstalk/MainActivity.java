739b12debe3e2c205dae3cd818fac898ab87c122
Ramesh Babu <ramesh@eternushealth.com>
2018-04-08 13:31:29 +0530

    Firebase authentication enabled

--- app/src/main/java/com/lkrb/beanstalk/MainActivity.java
+++ app/src/main/java/com/lkrb/beanstalk/MainActivity.java
@@ -1,13 +1,43 @@
 package com.lkrb.beanstalk;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG= "MainActivity >>> ";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword("s.rameshbabu1989@gmail.com", "Password@123")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.i(TAG, "onComplete: "+user.getEmail());
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            Log.i(TAG, "onComplete: fail");
+                        }
+                    }
+                });
     }
 }
