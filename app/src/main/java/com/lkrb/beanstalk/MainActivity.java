6102f5b7f836980d82a40f1db030511d0681c202
Ramesh Babu <ramesh@eternushealth.com>
2018-04-19 21:07:22 +0530

    Action bar changes

--- app/src/main/java/com/lkrb/beanstalk/MainActivity.java
+++ app/src/main/java/com/lkrb/beanstalk/MainActivity.java
@@ -45,14 +45,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -156,5 +148,10 @@ public class MainActivity extends AppCompatActivity
     protected void onStart() {
         super.onStart();
 
+    }
+
+    public void navigateToChat(View view) {
+        Intent i = new Intent(MainActivity.this, ChatActivity.class);
+        startActivity(i);
     }
 }
